50db8a6289d95e8476d9bdf29ca03c24915a285c
shaowei.wayne <shaowei.wayne@bytedance.com>
2018-09-16 10:37:06 +0800

    day 1

--- namespace/uts/main.go
+++ namespace/uts/main.go
@@ -1,5 +1,9 @@
 package main
 
+/*
+hostname
+*/
+
 import (
 	"os/exec"
 	"syscall"
@@ -8,15 +12,24 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("sh")
+	cmd := exec.Command("sh")  // 封装了os.StartProcess函数，实际是起新进程
 	cmd.SysProcAttr = &syscall.SysProcAttr {
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS,  // 指示clone()系统调用的参数，即创建哪些namespace
 	}
-	cmd.Stdin = os.Stdin
+	cmd.Stdin = os.Stdin  // 子命令的标准输出和输入
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		log.Fatal()
+		log.Fatal(err)
 	}
 }
+
+/*
+pstree -pl
+hosname -b
+*/
+
+// go 子进程启动过程：http://blog.51cto.com/allragedbody/1747146
+// readlink /proc/{pid}/ns/uts：软链到是内存数据
+// readlink 读取软链接：https://blog.csdn.net/diabloneo/article/details/7173438
